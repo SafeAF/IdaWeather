58269935c9034952d1c3212e4aa2c911313f1d42
safeaf <transiencymail@gmail.com>
2023-03-28 09:11:08 -0600

    graph form is working as expected. fields added or removed depending on category selected

--- dashboard/app/javascript/controllers/form_controller.js
+++ dashboard/app/javascript/controllers/form_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="form"
 
 export default class extends Controller {
-  static targets = ['startYear', 'endYear', 'year']
+  static targets = ['startYear', 'endYear', 'year', 'month']
  
 
  
@@ -40,8 +40,42 @@ export default class extends Controller {
    }
 
    toggle(){
-    console.log("barrrr")
-   
-   }
+    const selectedOption = document.querySelector('#foo').value
+    console.log(selectedOption)
+    
+   // console.log(yearField)
+   // yearField.style.display = 'block';
+   const yearField = document.querySelector('#yearField');
+   const monthField = document.querySelector('#monthField');
+   const startYearField = document.querySelector('#startYearField')
+   const endYearField = document.querySelector('#endYearField')
+
+
+   if (selectedOption === 'Range_across_years') {
+    startYearField.style.display = 'inline-block'
+    endYearField.style.display = 'inline-block'
+    
+    yearField.style.display = 'none'
+    monthField.style.display = 'none'
+
+  } else if (selectedOption === 'Compare_one_month_across_years') {
+    startYearField.style.display = 'inline-block'
+    endYearField.style.display = 'inline-block'
+    monthField.style.display = 'inline-block'
+    yearField.style.display = 'none'
+
+  } else if (selectedOption === 'Monthly_for_one_year') {
+    startYearField.style.display = 'none'
+    endYearField.style.display = 'none'
+    monthField.style.display = 'inline-block'
+    yearField.style.display = 'inline-block'
+
+  } else if (selectedOption === 'Hourly_for_one_month_in_a_single_year') {
+    startYearField.style.display = 'none'
+    endYearField.style.display = 'none'
+    monthField.style.display = 'inline-block'
+    yearField.style.display = 'inline-block'
+  }
+}
 }
  
