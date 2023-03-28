7955c0d65e3c402d693d2bd7f32e02fbdefff71a
safeaf <transiencymail@gmail.com>
2023-03-28 09:24:19 -0600

    graph form looking good, almost done

--- dashboard/app/javascript/controllers/form_controller.js
+++ dashboard/app/javascript/controllers/form_controller.js
@@ -4,40 +4,17 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ['startYear', 'endYear', 'year', 'month']
- 
 
- 
    connect() {
-     this.updateFields()
+    startYearField.style.display = 'inline-block'
+    endYearField.style.display = 'inline-block'
+    
+    yearField.style.display = 'none'
+    monthField.style.display = 'none'
+
    }
  
-   updateFields() {
-    console.log("foo")
-    const yearField = document.querySelector('#yearField');
-    console.log(yearField)
 
-  yearField.style.display = 'none';
-
-    const selectedOption = this.element.value
-    console.log(selectedOption)
-     if (selectedOption === 'Range_across_years') {
-       this.startYearTarget.style.display = 'inline-block'
-       this.endYearTarget.style.display = 'inline-block'
-       this.yearTarget.style.display = 'none'
-     } else if (selectedOption === 'Compare_one_month_across_years') {
-       this.startYearTarget.style.display = 'none'
-       this.endYearTarget.style.display = 'none'
-       this.yearTarget.style.display = 'inline-block'
-     } else if (selectedOption === 'Monthly_for_one_year') {
-       this.startYearTarget.style.display = 'inline-block'
-       this.endYearTarget.style.display = 'inline-block'
-       this.yearTarget.style.display = 'none'
-     } else if (selectedOption === 'Hourly_for_one_month_in_a_single_year') {
-       this.startYearTarget.style.display = 'inline-block'
-       this.endYearTarget.style.display = 'none'
-       this.yearTarget.style.display = 'inline-block'
-     }
-   }
 
    toggle(){
     const selectedOption = document.querySelector('#foo').value
@@ -67,7 +44,7 @@ export default class extends Controller {
   } else if (selectedOption === 'Monthly_for_one_year') {
     startYearField.style.display = 'none'
     endYearField.style.display = 'none'
-    monthField.style.display = 'inline-block'
+    monthField.style.display = 'none'
     yearField.style.display = 'inline-block'
 
   } else if (selectedOption === 'Hourly_for_one_month_in_a_single_year') {
