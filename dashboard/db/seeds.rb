458f3326c299619983bd18956a682db064a3b164
safeaf <transiencymail@gmail.com>
2023-03-29 16:28:08 -0600

    added uri gem in gemfile for production deploy

--- dashboard/db/seeds.rb
+++ dashboard/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+u = User.new
+u.email = "admin@idaweather.com"
+u.password = 'foobar'
+u.password_confirmation = 'foobar'
+u.save!
+
+s = Study.create(name: "The Winter of '85", 
+                 description: "The winter of 1985 was especially cold.")
+
+s2 = Study.create(name: "A Look at Precipitation in the Salmon Dam Watershed",
+                  description: "A look at the past 73 years of snowfall and precipitation in the mountains west of Jackpot")
